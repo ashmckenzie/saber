854a39da4b4b7e3142f8551a7f3ba4cb66fe6b22
Ash McKenzie <ash@the-rebellion.net>
2016-02-04 00:03:18 +1100

    Dual sound font support!

--- main/main.ino
+++ main/main.ino
@@ -18,22 +18,37 @@ int ZAxisValueDiff = 0;
 Timer t;
 
 int LED_PIN = 13;
+int SERIAL_RX_PIN = 4;
+int SERIAL_TX_PIN = 5;
 int MP3_BUSY_PIN = 12;
-int POWER_PIN = 8;
+int POWER_ON_OFF_PIN = 8;
+int SELECT_SOUND_FONT_PIN = 9;
 
-int MP3_VOLUME = 20;
+int MP3_VOLUME = 5;
 
-const int CLASH_THRESHOLD = 200;
+const int MAX_SOUND_FONT_FOLDERS = 2;
+const int SOUNDS_PER_SOUND_FONT_FOLDER = 6;
+
+const int CLASH_THRESHOLD = 300;
 const int SWING_THRESHOLD = 20;
 
-const int MOVEMENT_COUNT_THRESHOLD = 2;
+const int MOVEMENT_COUNT_THRESHOLD = 4;
 
-const int DEFAULT_DELAY_FOR_SABER_OFF = 500;
+const int DEFAULT_DELAY_FOR_SABER_OFF = 250;
 const int DEFAULT_DELAY_FOR_SABER_ON = 10;
 const int POWER_STATE_COUNT_THRESHOLD_WHEN_ON = 30;
 const int POWER_STATE_COUNT_THRESHOLD_WHEN_OFF = 1;
 
+const int BOOT_SOUND_NUM = 1;
+const int POWER_ON_SOUND_NUM = 2;
+const int POWER_OFF_SOUND_NUM = 3;
+const int HUM_SOUND_NUM = 4;
+const int SWING_SOUND_NUM = 5;
+const int CLASH_SOUND_NUM = 6;
+
 char str[1024];
+
+int currentSoundFontFolder = 0;
 
 int movementCount = 0;
 
@@ -48,6 +63,12 @@ int powerButtonStateCount = 0;
 int powerButtonStateCountThreshold = POWER_STATE_COUNT_THRESHOLD_WHEN_OFF;
 int delayFor = DEFAULT_DELAY_FOR_SABER_OFF;
 
+int selectSoundFontButtonState = 0;
+int selectSoundFontButtonStateCount = 0;
+int selectSoundFontButtonStateCountThreshold = 6;
+
+// Black Star
+//
 // 1 - boot
 // 2 - power on
 // 3 - power off
@@ -55,7 +76,22 @@ int delayFor = DEFAULT_DELAY_FOR_SABER_OFF;
 // 5 - swing
 // 6 - clash
 
-SoftwareSerial SerialMP3(4, 5); // RX, TX
+// EP IV
+//
+// 7  - boot
+// 8  - power on
+// 9  - power off
+// 10  - hum
+// 11 - swing
+// 12 - clash
+
+SoftwareSerial SerialMP3(SERIAL_RX_PIN, SERIAL_TX_PIN);
+
+void selectSoundFont(int num) {
+  if (num >= MAX_SOUND_FONT_FOLDERS) { num = 0; }
+  currentSoundFontFolder = num;
+  if (playBootSound) { playSound(BOOT_SOUND_NUM, 1890); }
+}
 
 void fireLED() {
   digitalWrite(LED_PIN, HIGH);
@@ -72,22 +108,33 @@ void setupMP3() {
   SerialMP3.begin(9600);
   mp3_set_serial(SerialMP3);
   delay(10);
+  
   mp3_set_device(1);
   delay(10);
+  
   mp3_set_volume(MP3_VOLUME);
   delay(10);
+  
   mp3_set_EQ(5);
   delay(10);
 
-  if (playBootSound) {
-    mp3_play_physical(1);
-    delay(1890);
-  }
+  selectSoundFont(currentSoundFontFolder);
+}
+
+void playSound(int num, int del) {
+  num = num + (SOUNDS_PER_SOUND_FONT_FOLDER * currentSoundFontFolder);
+  mp3_play_physical(num);
+  if (del > 0) { delay(del); }
 }
 
 void setupPowerSwitch() {
-  pinMode(POWER_PIN, INPUT);
-  digitalWrite(POWER_PIN, HIGH);
+  pinMode(POWER_ON_OFF_PIN, INPUT);
+  digitalWrite(POWER_ON_OFF_PIN, HIGH);
+}
+
+void setupChangeSoundFontSwitch() {
+  pinMode(SELECT_SOUND_FONT_PIN, INPUT);
+  digitalWrite(SELECT_SOUND_FONT_PIN, HIGH);
 }
 
 void setupLED() {
@@ -96,6 +143,7 @@ void setupLED() {
 }
 
 bool checkDiff(int threshold, int style) {
+  //
   // 0 = all
   // 1 = any
   //
@@ -123,17 +171,91 @@ void resetplayingSound() {
   playingSound = false;
 }
 
-void playSound(int num) {
+void playMovementSound(int num, int d) {
   if (!Serial.available()) {
     sprintf(str, "X=%d (%d), Y=%d (%d), Z=%d (%d)", XAxisValue, XAxisValueDiff, YAxisValue, YAxisValueDiff, ZAxisValue, ZAxisValueDiff);
     Serial.println(str);
   }
-  playingSound = true;
-  mp3_play_physical(num);
+
+  if (playMovementSounds) {
+    playingSound = true;
+    playSound(num, 0);  
+    t.after(d, resetplayingSound);
+  }
 }
 
 void repeatHum() {
-  mp3_repeat_play(4);
+  int num = HUM_SOUND_NUM + (SOUNDS_PER_SOUND_FONT_FOLDER * currentSoundFontFolder);
+  mp3_repeat_play(num);
+}
+
+void processPowerButton() {
+  powerButtonState = digitalRead(POWER_ON_OFF_PIN);
+
+  if (!powerButtonState) {
+    if (powerButtonStateCount >= powerButtonStateCountThreshold) {      
+      if (saberOn) {
+        // Turn OFF
+        saberOn = 0;
+        delayFor = DEFAULT_DELAY_FOR_SABER_OFF;
+        powerButtonStateCountThreshold = POWER_STATE_COUNT_THRESHOLD_WHEN_OFF;
+        if (playPowerSounds) {
+          playSound(POWER_OFF_SOUND_NUM, 1000);
+          mp3_stop();
+        }
+        if (!Serial.available()) { Serial.println("Power OFF!"); }
+      } else {
+        // Turn ON
+        saberOn = 1;        
+        delayFor = DEFAULT_DELAY_FOR_SABER_ON;
+        powerButtonStateCountThreshold = POWER_STATE_COUNT_THRESHOLD_WHEN_ON;        
+        if (playPowerSounds) {
+          playSound(POWER_ON_SOUND_NUM, 1000);
+          repeatHum();
+        }
+        if (!Serial.available()) { Serial.println("Power ON!"); }
+      }      
+      powerButtonStateCount = 0;      
+    } else {
+      powerButtonStateCount++;
+    }
+  }
+}
+
+void processChangeSoundFont() {
+  selectSoundFontButtonState = digitalRead(SELECT_SOUND_FONT_PIN);
+
+  if (!selectSoundFontButtonState) {
+    if (selectSoundFontButtonStateCount >= selectSoundFontButtonStateCountThreshold) {      
+      if (!Serial.available()) { Serial.println("Changing sound font!"); }
+      selectSoundFont(currentSoundFontFolder + 1);
+      selectSoundFontButtonStateCount = 0;      
+    } else {
+      selectSoundFontButtonStateCount++;
+    }
+  }
+}
+
+void processMovements() {      
+  if (XAxisValueLast > 0) { XAxisValueDiff = abs(XAxisValueLast - XAxisValue); }
+  if (YAxisValueLast > 0) { YAxisValueDiff = abs(YAxisValueLast - YAxisValue); }
+  if (ZAxisValueLast > 0) { ZAxisValueDiff = abs(ZAxisValueLast - ZAxisValue); }
+
+  if (!playingSound) {
+    if (checkDiff(CLASH_THRESHOLD, 1)) {
+      if (!Serial.available()) { Serial.println("CLASH!"); }
+      playMovementSound(CLASH_SOUND_NUM, 1060);
+    } else if (checkDiff(SWING_THRESHOLD, 0)) {
+      if (!Serial.available()) { Serial.println("SWING"); }
+       playMovementSound(SWING_SOUND_NUM, 600);      
+    }
+  }   
+}
+
+void captureAccelerometerValues() {
+  XAxisValue = analogRead(xAxis);
+  YAxisValue = analogRead(yAxis);
+  ZAxisValue = analogRead(zAxis);
 }
 
 /* ------------------------------------------------------------------------------------------------------------------------------------------------------- */
@@ -147,74 +269,21 @@ void setup() {
 
   setupLED();
   setupPowerSwitch();
+  setupChangeSoundFontSwitch();
   setupMP3();
 
   if (!Serial.available()) { Serial.println("Ready!"); }
 }
 
 void loop() {
-  powerButtonState = digitalRead(POWER_PIN);
+  captureAccelerometerValues();
 
-//  if (!Serial.available()) {
-//    sprintf(str, "powerButtonState=%d, saberOn=%d, delayFor=%d", powerButtonState, saberOn, delayFor);
-//    Serial.println(str);
-//  }
-
-  if (!powerButtonState) {
-    if (powerButtonStateCount >= powerButtonStateCountThreshold) {      
-      if (saberOn) {
-        // Turn OFF
-        saberOn = 0;
-        delayFor = DEFAULT_DELAY_FOR_SABER_OFF;
-        powerButtonStateCountThreshold = POWER_STATE_COUNT_THRESHOLD_WHEN_OFF;
-        if (playPowerSounds) {
-          mp3_play_physical(3);
-          delay(1000);
-          mp3_stop();
-        }
-      } else {
-        // Turn ON
-        saberOn = 1;        
-        delayFor = DEFAULT_DELAY_FOR_SABER_ON;
-        powerButtonStateCountThreshold = POWER_STATE_COUNT_THRESHOLD_WHEN_ON;        
-        if (playPowerSounds) {
-          mp3_play_physical(2);
-          delay(1000);
-          repeatHum();
-        }
-      }      
-      powerButtonStateCount = 0;      
-    } else {
-      powerButtonStateCount++;
-    }
-  }
+  processPowerButton();
   
   if (saberOn) {
-    
-    XAxisValue = analogRead(xAxis);
-    YAxisValue = analogRead(yAxis);
-    ZAxisValue = analogRead(zAxis);
-  
-    if (XAxisValueLast > 0) { XAxisValueDiff = abs(XAxisValueLast - XAxisValue); }
-    if (YAxisValueLast > 0) { YAxisValueDiff = abs(YAxisValueLast - YAxisValue); }
-    if (ZAxisValueLast > 0) { ZAxisValueDiff = abs(ZAxisValueLast - ZAxisValue); }
-  
-
-    if (!playingSound) {
-      if (checkDiff(CLASH_THRESHOLD, 1)) {
-        if (!Serial.available()) { Serial.println("CLASH!"); }
-        if (playMovementSounds) {
-          playSound(6);
-          t.after(1060, resetplayingSound);
-        }
-      } else if (checkDiff(SWING_THRESHOLD, 0)) {
-        if (!Serial.available()) { Serial.println("SWING"); }
-        if (playMovementSounds) {
-          playSound(5);
-          t.after(600, resetplayingSound);
-        }
-      }
-    }   
+    processMovements();
+  } else {
+    processChangeSoundFont();  
   }
 
   XAxisValueLast = XAxisValue;
